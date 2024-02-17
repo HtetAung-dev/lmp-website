81d94e9c22e1c81e3562befb40d31df868715ab2
LarryPinicola <thelarry1549@gmail.com>
2024-02-17 11:04:08 +0630

    [inProgress] add background about section

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Hero />
       </div>
       <Service />
-      <About className="aboutImg" />
+      <About className="" />
       <Newsletter className="footerImg" />
     </div>
   );
