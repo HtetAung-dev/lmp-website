2697ebb26a0c9434741e2b9dd2484b04b782b6ac
LarryPinicola <thelarry1549@gmail.com>
2024-02-17 10:31:47 +0630

    [complete] copyright

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -5,9 +5,9 @@ import Social from './Social'
 const Newsletter = () => {
 
     return (
-        <div className='w-full py-16 text-white px-4 footerImg border' id='contact'>
+        <div className='w-full py-16 text-white px-4 footerImg' id='contact'>
             {/* max-w-[850px]  */}
-            <div className='lg:w-[90%] mx-auto grid lg:grid-cols-4 gap-5 lg:px-5'> 
+            <div className='lg:w-[90%] mx-auto grid lg:grid-cols-4 gap-5 lg:px-5'>
                 <div className='lg:col-span-2 px-4 py-4'>
                     <h1 className='md:text-2xl sm:text-xl text-xl font-bold py-2 text-justify'>Want tips & advices to upgrade  your work?</h1>
                     <p className='text-justify'>Leave your mail address here to get the conversation with our business development team.</p>
@@ -24,11 +24,10 @@ const Newsletter = () => {
                     <div>
                         <Social />
                     </div>
-
                 </div>
-
-
-
+            </div>
+            <div className="text-sm text-center text-green-400 my-2 font-semibold">
+                &copy; {new Date().getFullYear()} LMP Corporation. All rights reserved.
             </div>
         </div>
     )
--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -6,7 +6,7 @@ const Social = () => {
             {/* Social links */}
             <div className='mt-5 flex items-center justify-around w-[70%] mx-auto'>
                 <p className='lg:text-start mb-2'>Get in touch</p>
-                <ul className='flex justify-between lg:w-36'>
+                <ul className='flex justify-between lg:w-36 gap-2'>
                     <li className="w-9 h-9 items-center flex justify-center rounded-full bg-black hover:scale-125 duration-150">
                         <a href="https://www.facebook.com" target="_blank">
                             <i className="text-2xl ri-facebook-circle-fill"></i>
