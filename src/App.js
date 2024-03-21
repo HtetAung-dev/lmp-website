e54ea095d3a8d591f754a522ef1093896e16c314
HtetAung-dev <htetaung.dev@gmail.com>
2024-03-21 23:02:30 +0630

    added SEO optimization

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import Service from './Components/Service';
 import Newsletter from './Components/Newsletter';
 import About from './Components/About';
 import Footer from './Components/Footer';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
     <div className="App">
+      <HelmetProvider>
+        <title>LMP Co.,Ltd</title>
+        <meta name="description" content="Welcome to Luu Min Phyo Co.,Ltd" />
+        <link rel="canonical" href='/' />
+      </HelmetProvider>
       <div className='w-100 navcontainer'>
         <div className='navbg'></div>
         <Navbar />
